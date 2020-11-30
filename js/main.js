7aca92faed5451b342314532d8ece4b4e648b1d0
Jun-Honda-0326 <60055417+Jun-Honda-0326@users.noreply.github.com>
2020-12-01 06:27:22 +0900

    タスクの完了、未完了

--- js/main.js
+++ js/main.js
@@ -8,7 +8,8 @@ var app = new Vue({
         addItem: function() {
             if (this.newItem == '') return; //もし未入力なら処理を終了させる
             var todo = {
-                item: this.newItem
+                item: this.newItem,
+                isDone: false
             };
             this.todos.push(todo);
             this.newItem = '';
